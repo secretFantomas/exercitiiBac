72f20310b91c3f153ea1fa70a9cdf91a05ac0901
fantomas <tt@tt>
2018-04-17 15:24:38 +0300

    add S3_2a

--- bac2007/src/main/java/Var003/S3_2a.java
+++ bac2007/src/main/java/Var003/S3_2a.java
@@ -1,4 +1,16 @@
 package Var003;
 
+import java.util.ArrayList;
+
 public class S3_2a {
+
+    public int receivedNr(int number){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (number!=0) {
+            number = number / 10;
+            list.add(number);
+        }
+        return list.size();
+    }
 }
