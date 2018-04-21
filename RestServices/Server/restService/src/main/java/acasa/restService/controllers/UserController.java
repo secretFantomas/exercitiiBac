6adac9ee6d293f296a36d839f3efa63f707eea7e
user <user>
2018-04-21 09:27:28 +0300

    added rest method to set the user

--- RestServices/Server/restService/src/main/java/acasa/restService/controllers/UserController.java
+++ RestServices/Server/restService/src/main/java/acasa/restService/controllers/UserController.java
@@ -1,7 +1,9 @@
 package acasa.restService.controllers;
 
+import acasa.restService.User;
 import acasa.restService.UserDataBaseEmulator;
 import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,26 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String getUser(){
-        return new Gson().toJson(UserDataBaseEmulator.getUser());
+        User user = UserDataBaseEmulator.getUser();
+        String message = new Gson().toJson(user);
+        return message;
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String setUser(@RequestBody String message){
+
+        User user = null;
+        try {
+            user = new Gson().fromJson(message, User.class);
+        }catch(Exception e){
+            return "Unable to deserialize request body to user. More details: " + e.getMessage();
+        }
+        UserDataBaseEmulator.setUser(user);
+
+
+        return "Done";
+    }
+
+    
 
 }
