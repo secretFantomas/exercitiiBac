adb0a84799379fb068064792c3751887015b7c25
fantomas <tt@tt>
2018-04-27 15:45:51 +0300

    added var006

---
+++ bac2007/src/main/java/Var004/S3_3.java
@@ -0,0 +1,15 @@
+package Var004;
+
+public class S3_3 {
+    //este scrisa o expresie aritmetica fara paranteze care la sfarsit are =
+    // input: 2+3-4+6-3+5=
+    //output: 2+3-4+6-3+5=10
+
+    public String expresieAritmetica(String input){
+        String result=null;
+
+        return result;
+
+    }
+
+}
---
+++ bac2007/src/test/java/Var004/S3_3Test.java
@@ -0,0 +1,15 @@
+package Var004;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class S3_3Test {
+
+    @Test
+    public void expresieAritmetica() {
+
+        String input="2+3-4+6-3+5=";
+        String expectedResult = "2+3-4+6-3+5=10";
+    }
+}
