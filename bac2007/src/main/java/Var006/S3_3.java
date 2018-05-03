f08de38511c880b261d7f007019df9e36d107c05
fantomas <tt@tt>
2018-05-03 09:52:01 +0300

    added var006 S3_3

---
+++ bac2007/src/main/java/Var006/S3_3.java
@@ -0,0 +1,13 @@
+package Var006;
+
+public class S3_3 {
+    //Sa se afiseze lungimea maxima a secventelor strict pozitive
+    //input dimensiunea sirului 10
+    //input 7 22 -3 10 3 14 0 21 10 8 (Avem mai multe secvente: 7 22; 10 3 14 si 21 10 8)
+    //output 3
+
+    public void checkSecquenceDimension(){
+
+    }
+
+}
