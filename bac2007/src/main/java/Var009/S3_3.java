0962d64b73dcb40352bd6e1bf6edb49edd760451
fantomas <tt@tt>
2018-05-04 10:03:37 +0300

    resolved S3_3 from var009

--- bac2007/src/main/java/Var009/S3_3.java
+++ bac2007/src/main/java/Var009/S3_3.java
@@ -6,7 +6,33 @@ public class S3_3 {
     //sa se afiseze daca sirul contine sau nu creasta
 
     public Integer verificaCreasta(ArrayList<Integer> list){
-        return 0;
+         int creasta=0;
+         int pozitieCreasta = 0;
+         boolean eCreasta=false;
+
+        for (int i=0; i<list.size()-1;i++) {
+            if (!(list.get(i + 1) > list.get(i)))
+                break;
+            else {
+                creasta = list.get(i + 1);
+                pozitieCreasta = i + 1;
+            }
+        }
+
+        for (int i=pozitieCreasta; i<list.size()-1;i++){
+            if (list.get(i + 1) < list.get(i))
+                eCreasta=true;
+            else
+            {
+                eCreasta=false;
+                break;
+            }
+        }
+
+        if(eCreasta)
+            return creasta;
+        else
+            return 0;
 
     }
 
--- bac2007/src/test/java/Var009/S3_3Test.java
+++ bac2007/src/test/java/Var009/S3_3Test.java
@@ -10,8 +10,24 @@ import static org.junit.Assert.*;
 public class S3_3Test {
 
     @Test
-    public void verificaCreastaTest() {
+    public void verificaECreastaTest() {
+        S3_3 subject = new S3_3();
 
-        ArrayList<Integer> input
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(7,12,17,26,15,3));
+        int receivedResult = subject.verificaCreasta(input);
+        int expectedResult = 26;
+
+        assertEquals(expectedResult,receivedResult);
+    }
+
+    @Test
+    public void verificaNuECreastaTest() {
+        S3_3 subject = new S3_3();
+
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(7,12,17,10,15,3));
+        int receivedResult = subject.verificaCreasta(input);
+        int expectedResult = 0;
+
+        assertEquals(expectedResult,receivedResult);
     }
 }
