9b3abfbb14dd0299139bc36d8fdf4b481b7e72fc
fantomas <tt@tt>
2018-10-31 14:38:16 +0200

    Add new app (timer between 2 activities)

---
+++ Android/1. TimerAppBetween2Activities/app/src/main/java/com/example/isabelapuiu/twoactivitieswithtimer/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.isabelapuiu.twoactivitieswithtimer;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button nextBtn, startBtn;
+    private CountDownTimer countDownTimer;
+    private TextView timerText;
+    private long timeLeftInMillieSeconds = 600000; // 10 min
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nextBtn = (Button) findViewById(R.id.nextBtn);
+        startBtn = (Button) findViewById(R.id.startBtn);
+        timerText = (TextView) findViewById(R.id.timerText);
+
+       // updateTimer();
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer = new CountDownTimer(timeLeftInMillieSeconds, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        timeLeftInMillieSeconds = millisUntilFinished;
+                        updateTimer();
+                    }
+
+                    @Override
+                    public void onFinish() {
+
+                    }
+                }.start();
+
+            }
+        });
+    }
+
+    private void updateTimer() {
+        int minutes = (int) timeLeftInMillieSeconds / 60000;
+        int seconds = (int) timeLeftInMillieSeconds % 60000 / 1000;
+
+        String timeLeftText;
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+
+        if (seconds < 10) {
+            timeLeftText += "0";
+
+        } else {
+            timeLeftText += seconds;
+        }
+
+        timerText.setText(timeLeftText);
+    }
+}
